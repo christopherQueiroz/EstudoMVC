645a6c6f74cebccc46e919c9d3b590b1fc8887c8
TARDIS <TARDIS>
2016-04-03 22:09:27 -0300

    Initial Commit

---
+++ RotasMVC/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using RotasMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RotasMVC.Controllers
+{
+
+    //HomeController herda de Controller
+    public class HomeController : Controller
+    {
+
+        //criação de uma variável todasAsNoticias que é do tipo IEnumerable<Params>
+        private readonly IEnumerable<Noticia> todasAsNoticias;
+
+        //O Controller Home
+        public HomeController() 
+        {
+            //o objeto todasAsNoticias recebe o conteúdo do método TodasAsNoticias, da nossa Model Noticia, ordenado pela data
+            todasAsNoticias = new Noticia().TodasAsNoticias().OrderByDescending(x => x.Data);
+        }
+
+        //Action Index
+        public ActionResult Index()
+        {
+
+            //Aqui ultimasNoticias recebe as três primeiras noticias armazenadas em todasAsNoticias.
+            var ultimasNoticias = todasAsNoticias.Take(3);
+
+            //a variável todasAsCategorias recebe a lista distinta das categorias em todasAsNoticias
+            var todasAsCategorias = todasAsNoticias.Select(x => x.Categoria).Distinct().ToList();
+
+            //A ViweBag.Categoria recebe todasAsCategorias
+            ViewBag.Categoria = todasAsCategorias;
+
+            //Aqui retornamos ultimasNoticias na view.
+            return View(ultimasNoticias);
+        }
+
+
+        //Action TodasAsNoticias(). Não confundir com o método TodasAsNoticias() da Model. Esse é um ActionResult, não um IEnumerable
+        public ActionResult TodasAsNoticias()
+        {
+            //Retorna todasAsNoticias na view
+            return View(todasAsNoticias);
+        }
+
+
+        //Action MostraNoticia, recebe por parâmetro o id da noticia, seu título e sua categoria.
+        public ActionResult MostraNoticia(int noticiaId, string titulo, string categoria)
+        {
+            //Retorna na View o primeiro ou o default do conteúdo de todasAsNoticias onde o id corrente da noticia da Model for igual ao parâmetro noticiaId, passado na view
+            return View(todasAsNoticias.FirstOrDefault(x => x.NoticiaId == noticiaId));
+        }
+
+        //Action MostraCategoria, que recebe uma string que é a categoria selecionada
+        public ActionResult MostraCategoria(string categoria)
+        {
+
+            //A variável categoriaEspecífica recebe o conteúdo de todasAsNocicias onde o valor corrente da categoria na Model for igual ao valor de categoria passado por parâmetro
+            var categoriaEspecifica = todasAsNoticias.Where(x => x.Categoria.ToLower() == categoria.ToLower()).ToList();
+
+            //A ViewBag.Categoria recebe o parâmetro da action, que é a string categoria.
+            ViewBag.Categoria = categoria;
+
+            //E o retorno é o conteúdo de categoriaEspecifica na view.
+            return View(categoriaEspecifica);
+        }
+    } 
+}
---
+++ RotasMVC/Models/Noticia.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RotasMVC.Models
+{
+    public class Noticia
+    {
+
+        //Propriedades da classe Noticia, da nossa Model
+        public int NoticiaId { get; set; }
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public string Categoria { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Data { get; set; }
+
+        //Criação do objeto TodasAsNoticias, do tipo IEnumerable<Params>, que permite armazenar objetos.
+        public IEnumerable<Noticia> TodasAsNoticias()
+        {
+
+            //Retorno recebe uma Collection de Noticia
+            var retorno = new Collection<Noticia>
+            {
+
+                //Os objetos Noticia, inseridos na collection retorno, são criados
+                new Noticia
+                {
+                    NoticiaId =1,
+                    Categoria = "Esportes",
+                    Titulo = "Campeonato de Punheta",
+                    Conteudo = "Jovens de São Paulo organizam o primeiro campeonato de punheta, cujos critérios vão de coloração até volume e distância do jato",
+                    Data = new DateTime(2015,12,11)
+                },
+
+                  new Noticia
+                {
+                    NoticiaId =2,
+                    Categoria = "Culinaria",
+                    Titulo = "Palmirinha cheira polvilho",
+                    Conteudo = "Palmirinha coloca cocaína nos bolinhos de polvilho confundindo com a farinha e é internada por aspiração de glutem",
+                    Data = new DateTime(2012,3,6)
+                },
+
+                  new Noticia
+                {
+                    NoticiaId =3,
+                    Categoria = "Cinema",
+                    Titulo = "Larissa Transante",
+                    Conteudo = "Garota de programa da região central da capital paulista, Larissa Olivério conta a trama de sua vida. 'Foi foda. Adoro' - Diz",
+                    Data = new DateTime(2016,6,3)
+                }
+
+
+            };
+
+
+            //O retorno de TodasAsNoticias() é retorno, que é a tal da collection com todos os objetos "NOticia"
+            return retorno;
+
+        }
+    }
+}
